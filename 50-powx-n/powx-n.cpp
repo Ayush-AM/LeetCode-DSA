fbf5750917434c786cc9485d0f7af7e231c1bbf5
Ayush Mahajan <140263932+Ayush-AM@users.noreply.github.com>
2026-05-20 12:54:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (92.43%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    double solve(double x, long n){
+        if(n==0) return 1;
+        if(n<0) return solve(1/x,-n);
+        if(n%2 == 0) return solve(x*x, n/2);
+
+        return x*solve(x*x, (n-1)/2);
+    }
+    double myPow(double x, int n) {
+        return solve(x, (long)n);
+    }
+};
+
+
+/*
+        if(n==0){
+            return 1.0;
+        }
+        if(x==0){
+            return 0.0;
+        }
+        if(x==1){
+            return 1.0;
+        }
+        if(x==-1 && n%2 == 0){
+            return 1.0;
+        }
+        if(x==-1 && n%2 != 0){
+            return -1.0;
+        }
+        long binForm = n;
+        if(n<0){
+            x = 1/x;
+            binForm = -binForm;
+        }
+        double ans = 1;
+
+        while(binForm > 0){
+            if(binForm % 2 == 1){
+                ans *= x;
+            }
+            x *= x;
+            binForm /= 2;
+        }
+        return ans;
+*/
