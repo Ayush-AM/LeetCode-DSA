3ca6fda804f1950874ddb4452513d362e44bdb49
Ayush Mahajan <140263932+Ayush-AM@users.noreply.github.com>
2026-05-26 12:42:27 +0530

    Time: 0 ms (100%), Space: 11.6 MB (47.43%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low=0;
-        int medium=0;
-        int high=nums.size()-1;
-        while(medium<=high){
-            if(nums[medium]==0){
-                swap(nums[low],nums[medium]);
-                low++,medium++;
-            }
-            else if(nums[medium]==1){
-                medium++;
-            }
-            else{
-                swap(nums[medium],nums[high]);
-                high--;
+        int lo = 0;
+        int mid = 0;
+        int hi = nums.size() - 1;
+
+        while (mid <= hi) {
+            switch (nums[mid]) {
+                // If the element is 0, group it at the beginning
+                case 0:
+                    swap(nums[lo++], nums[mid++]);
+                    break;
+                
+                // If the element is 1, it's already in the right middle zone
+                case 1:
+                    mid++;
+                    break;
+                
+                // If the element is 2, group it at the end
+                case 2:
+                    swap(nums[mid], nums[hi--]); // Fixed: Decrement hi to narrow down the boundary
+                    break;
             }
         }
-
     }
 };
