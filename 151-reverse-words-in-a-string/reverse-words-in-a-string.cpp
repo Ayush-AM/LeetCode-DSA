874778028f4297676c06488f2e2508db0b1b5800
Ayush Mahajan <140263932+Ayush-AM@users.noreply.github.com>
2026-05-27 13:21:14 +0530

    Time: 3 ms (36.04%) | Memory: 11.2 MB (23.31%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
    string reverseWords(string s) {
-    int n = s.length();
-    string ans = "";
-    reverse(s.begin(), s.end());
-    for(int i = 0; i<n; i++){
-        string word = "";
-        while(i < n && s[i] != ' ') {
-            word += s[i];
-            i++;
-        }
-        reverse(word.begin(), word.end());
-        if (word.length() > 0) {
-            ans += " " + word;
+    // int n = s.length();
+    // string ans = "";
+    // reverse(s.begin(), s.end());
+    // for(int i = 0; i<n; i++){
+    //     string word = "";
+    //     while(i < n && s[i] != ' ') {
+    //         word += s[i];
+    //         i++;
+    //     }
+    //     reverse(word.begin(), word.end());
+    //     if (word.length() > 0) {
+    //         ans += " " + word;
+    //     }
+    // }
+    // return ans.substr(1);
+
+    vector<string> words;
+    string w = "";
+
+    for(char ch: s){
+        if(ch!=' '){
+            w += ch;
+        }else if(!w.empty()){
+            words.push_back(w);
+            w = "";
         }
     }
-    return ans.substr(1);
+    if(!w.empty()){
+        words.push_back(w);
+    }
+    reverse(words.begin(), words.end());
+    string ans = "";
+
+    for(int i = 0; i<words.size(); i++){
+        ans += words[i];
+        if(i != words.size()-1) ans += " ";
+    }
+    return ans;
   }
 };
