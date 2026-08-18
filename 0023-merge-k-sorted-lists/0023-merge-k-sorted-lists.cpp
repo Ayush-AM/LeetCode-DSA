07c33bce22864a3edf082c1b08361fc8a1236cb3
Ayush Mahajan <140263932+Ayush-AM@users.noreply.github.com>
2026-08-18 21:48:39 +0530

    Time: 4 ms (42.21%), Space: 18.9 MB (24.35%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,29 +10,58 @@
  */
 class Solution {
 public:
-    bool operator()(ListNode* a, ListNode* b){
-        return a->val > b->val;
-    }
+    // bool operator()(ListNode* a, ListNode* b){
+    //     return a->val > b->val;
+    // }
 
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode* , vector<ListNode*> , Solution> minHeap;
-        
-        for(auto list : lists){
-            if(list) minHeap.push(list);
-        }
-        ListNode* dummy = new ListNode(-1);
-        ListNode* tail = dummy;
-
-        while(!minHeap.empty()){
-            ListNode* node = minHeap.top();
-            minHeap.pop();
-            tail -> next = node;
-            tail = tail -> next;
-
-            if(node -> next){
-                minHeap.push(node->next);
+        vector<int> result;
+        for(ListNode* current : lists){
+            while(current != nullptr){
+                result.push_back(current->val);
+                current = current->next;
             }
         }
-        return dummy->next;
+        sort(result.begin(),result.end());
+        
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        for(int value : result){
+            tail->next = new ListNode(value);
+            tail= tail->next;
+        }
+        return dummy.next;
+
+
     }
 };
+
+// class Solution {
+// public:
+//     bool operator()(ListNode* a, ListNode* b){
+//         return a->val > b->val;
+//     }
+
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+//         priority_queue<ListNode* , vector<ListNode*> , Solution> minHeap;
+        
+//         for(auto list : lists){
+//             if(list) minHeap.push(list);
+//         }
+//         ListNode* dummy = new ListNode(-1);
+//         ListNode* tail = dummy;
+
+//         while(!minHeap.empty()){
+//             ListNode* node = minHeap.top();
+//             minHeap.pop();
+//             tail -> next = node;
+//             tail = tail -> next;
+
+//             if(node -> next){
+//                 minHeap.push(node->next);
+//             }
+//         }
+//         return dummy->next;
+//     }
+// };
