f854a33f6354ce95b5c1d2842bca73290e2b15fe
Ayush Mahajan <140263932+Ayush-AM@users.noreply.github.com>
2026-08-26 02:41:30 +0530

    Time: 1 ms (6.48%) | Memory: 8.5 MB (54.38%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,35 @@
+// class Solution {
+// public:
+
+//     int rec(vector<int> &dp, int n){
+//         if(n == 0 || n==1 || n==2 || n==3) return n;
+
+//         if(dp[n] != -1){return dp[n];}
+
+//         return dp[n] = rec(dp, n-1) + rec(dp, n-2);
+//     }
+
+//     int climbStairs(int n) {
+//         vector<int> dp(n+1,-1);
+//         return rec(dp, n);
+//     }
+// };
+
 class Solution {
 public:
 
-    int rec(vector<int> &dp, int n){
-        if(n == 0 || n==1 || n==2 || n==3) return n;
-
-        if(dp[n] != -1){return dp[n];}
-
-        return dp[n] = rec(dp, n-1) + rec(dp, n-2);
-    }
 
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return rec(dp, n);
+        if(n == 0 || n==1 || n==2 || n==3) return n;
+        vector<int> dp(n+1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
     }
 };
