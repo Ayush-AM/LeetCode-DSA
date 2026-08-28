6d59bca49289345f9240ab1567cabd9b1de25b63
Ayush Mahajan <140263932+Ayush-AM@users.noreply.github.com>
2026-08-28 16:39:41 +0530

    Time: 7 ms (2.11%), Space: 10.2 MB (5.11%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,5 +1,17 @@
 class Solution {
 public:
+    int fun(string s, int i, int sign, long long num){
+        if(i>=s.size() || !isdigit(s[i])){
+            return num*sign;
+        }
+        num = num * 10 + (s[i]-'0');
+
+        if (sign * num > INT_MAX) return INT_MAX;
+        if (sign * num < INT_MIN) return INT_MIN;
+            
+        return fun(s,i+1,sign,num);
+    }
+
     int myAtoi(string s) {
         int i = 0;
         int n = s.length();
@@ -16,18 +28,7 @@ public:
             i++;
         }
         
-        // 3. Convert digits and handle overflow
-        long result = 0;
-        while (i < n && isdigit(s[i])) {
-            result = result * 10 + (s[i] - '0');
-            
-            // Check 32-bit range limits immediately
-            if (sign == 1 && result > INT_MAX) return INT_MAX;
-            if (sign == -1 && -result < INT_MIN) return INT_MIN;
-            
-            i++;
-        }
         
-        return (int)(result * sign);
+        return fun(s, i, sign, 0);
     }
 };
