944bf6d5ff2abaa24693e8ab6fefeac8991489f3
Ayush Mahajan <140263932+Ayush-AM@users.noreply.github.com>
2026-09-22 13:44:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (58.98%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // If the array has 2 or fewer elements, it's already valid.
-        if (nums.size() <= 2) {
+        if(nums.size() < 2){
             return nums.size();
         }
-        
-        // 'i' points to the index where the next valid element will be placed.
-        int i = 2; 
-        
-        // Start iterating from the 3rd element (index 2)
-        for (int j = 2; j < nums.size(); j++) {
-            // Check if current element is different from the element 2 places behind 'i'
-            if (nums[j] != nums[i - 2]) {
-                nums[i] = nums[j];
-                i++; // Move the valid pointer forward
+
+        int slow = 2;
+        for(int fast = 2; fast < nums.size(); fast++){
+            if(nums[fast] != nums[slow - 2]){
+                nums[slow] = nums[fast];
+                slow ++;
             }
         }
-        
-        // 'i' represents the count of valid elements
-        return i;
+        return slow;
     }
 };
